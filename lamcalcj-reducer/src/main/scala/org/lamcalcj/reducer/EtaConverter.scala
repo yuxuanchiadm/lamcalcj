38472eeb7dca63dc64b43170a627a0d383afcaad
yuxuanchiadm <yuxuanchiadm@126.com>
2018-12-15 00:22:08 +0800

    Refactor eta converter using trampoline

--- lamcalcj-reducer/src/main/scala/org/lamcalcj/reducer/EtaConverter.scala
+++ lamcalcj-reducer/src/main/scala/org/lamcalcj/reducer/EtaConverter.scala
@@ -2,11 +2,12 @@ package org.lamcalcj.reducer
 
 import org.lamcalcj.ast.Lambda._
 import org.lamcalcj.utils.Utils
+import org.lamcalcj.utils.Trampoline._
 
 object EtaConverter {
   def etaConversion(term: Term, maxStep: Int = Int.MaxValue, headOnly: Boolean = false, evaluationOnly: Boolean = false): (Boolean, Term) = {
     val etaConverter: EtaConverter = new EtaConverter(maxStep, headOnly, evaluationOnly)
-    val result: Term = etaConverter.convert(term)
+    val result: Term = etaConverter.convert(term).runT
     return (!etaConverter.aborted, result)
   }
 
@@ -14,22 +15,28 @@ object EtaConverter {
     var step: Int = 0
     var aborted: Boolean = false
 
-    def convert(inputTerm: Term): Term = if (aborted) inputTerm else
+    def convert(inputTerm: Term): Trampoline[Term] = if (aborted) Done(inputTerm) else
       inputTerm match {
-        case Var(identifier) => Var(identifier)
+        case Var(identifier) => Done(Var(identifier))
         case Abs(variable, App(term, Var(identifier))) =>
           if (variable.identifier == identifier && !Utils.hasFreeOccurrence(term, identifier))
             if (step >= maxStep) {
               aborted = true
-              inputTerm
+              Done(inputTerm)
             } else {
               step += 1
-              term
+              Done(term)
             }
-          else
-            Abs(variable, App(if (evaluationOnly) term else convert(term), Var(identifier)))
-        case Abs(variable, term) => Abs(variable, if (evaluationOnly) term else convert(term))
-        case App(term, argument) => App(convert(term), if (headOnly) argument else convert(argument))
+          else for {
+            currentTerm <- convert(term)
+          } yield Abs(variable, App(if (evaluationOnly) term else currentTerm, Var(identifier)))
+        case Abs(variable, term) => for {
+          currentTerm <- convert(term)
+        } yield Abs(variable, if (evaluationOnly) term else currentTerm)
+        case App(term, argument) => for {
+          currentTerm <- convert(term)
+          currentArgument <- convert(argument)
+        } yield App(currentTerm, if (headOnly) argument else currentArgument)
       }
   }
 }

839ef71ae3626794292fc7522b878a751137c97b
yuxuanchiadm <yuxuanchiadm@126.com>
2019-04-01 11:34:03 +0800

    Add interruptible trampoline

--- lamcalcj-utils/src/main/scala/org/lamcalcj/utils/Trampoline.scala
+++ lamcalcj-utils/src/main/scala/org/lamcalcj/utils/Trampoline.scala
@@ -1,7 +1,10 @@
 package org.lamcalcj.utils
 
+import scala.annotation.tailrec
+
 object Trampoline {
   sealed trait Trampoline[+A] {
+    @tailrec
     final def resume: Either[() => Trampoline[A], A] = this match {
       case Done(v) => Right(v)
       case More(k) => Left(k)
@@ -12,9 +15,37 @@ object Trampoline {
       }
     }
 
+    @tailrec
     final def runT: A = resume match {
       case Right(a) => a
       case Left(k) => k().runT
+    }
+
+    @throws[InterruptedException]
+    @tailrec
+    final def interruptibleResume: Either[() => Trampoline[A], A] = {
+      if (Thread.interrupted())
+        throw new InterruptedException();
+      this match {
+        case Done(v) => Right(v)
+        case More(k) => Left(k)
+        case FlatMap(a, f) => a match {
+          case Done(v) => f(v).interruptibleResume
+          case More(k) => Left(() => k() flatMap f)
+          case FlatMap(b, g) => b.flatMap((x: Any) => g(x) flatMap f).interruptibleResume
+        }
+      }
+    }
+
+    @throws[InterruptedException]
+    @tailrec
+    final def interruptibleRunT: A = {
+      if (Thread.interrupted())
+        throw new InterruptedException();
+      interruptibleResume match {
+        case Right(a) => a
+        case Left(k) => k().interruptibleRunT
+      }
     }
 
     final def flatMap[B](f: A => Trampoline[B]): Trampoline[B] = this match {
